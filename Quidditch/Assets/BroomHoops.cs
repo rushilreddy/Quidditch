153db4e4b953cb9380dd052584d736be9fa1c306
Roosh <rushilvreddy@gmail.com>
2016-07-13 15:37:36 -0700

    cool

---
+++ Quidditch/Assets/BroomHoops.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BroomHoops : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("hello1");
+        if (col.tag == "Hoop")
+        {
+            var a = GameManager.instance;
+            a.nextHoop();
+        }
+    }
+}
---
+++ Quidditch/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] hoops;
+    public int cHoop = 0;
+    public static GameManager instance;
+
+
+    // Use this for initialization
+    void Start () {
+        instance = this;
+
+    }
+
+    // Update is called once per frame
+    void Update () { 
+
+
+	}
+
+    public void nextHoop()
+    {
+        Debug.Log("NEXTHOOp");
+        hoops[cHoop].GetComponent<Renderer>().enabled = false;
+        ++cHoop;
+        hoops[cHoop].GetComponent<Renderer>().material.color = Color.green;
+
+        //hoops[cHoop].mater
+    }
+}
